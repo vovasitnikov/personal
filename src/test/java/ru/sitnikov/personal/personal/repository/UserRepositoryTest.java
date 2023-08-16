2b8f4c51dfb41da6c66f47ac11ecc4107f292c11
vovasitnikov <vovasitnikov1209@yandex.ru>
2023-08-16 17:17:55 +0400

    тесты

--- src/test/java/ru/sitnikov/personal/personal/repository/UserRepositoryTest.java
+++ src/test/java/ru/sitnikov/personal/personal/repository/UserRepositoryTest.java
@@ -3,7 +3,9 @@ package ru.sitnikov.personal.personal.repository;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.jdbc.Sql;
 import ru.sitnikov.personal.personal.model.UserModel;
 
 import java.util.Optional;
@@ -11,6 +13,8 @@ import java.util.Optional;
 import static org.junit.jupiter.api.Assertions.*;
 @DataJpaTest
 @ActiveProfiles("test")
+@DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_EACH_TEST_METHOD)
+@Sql("/data.sql")
 class UserRepositoryTest {
 
     @Autowired
@@ -19,6 +23,11 @@ class UserRepositoryTest {
 
     @Test
     void createUser() {
+        UserModel userModel = new UserModel();
+        userModel.setName("Bob");
+        userModel.setLastName("Jones");
+        UserModel saved  = userRepository.save(userModel);
+        assertNotNull(saved);
     }
 
     @Test
